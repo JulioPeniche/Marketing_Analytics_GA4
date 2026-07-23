2135fd4242613c4688c506d77a959c1fa756af54
Júlio Peniche <76125283+JulioPeniche@users.noreply.github.com>
2026-07-23 10:23:25 -0300

    Add SQL queries for top products and categories
    Added SQL queries to retrieve top products and categories by revenue and quantity sold.

--- sql/06_produtos.sql
+++ sql/06_produtos.sql
@@ -1,1 +1,108 @@
 
+
+-- Top 10 produtos por receita
+
+SELECT
+
+items.item_name,
+
+ROUND(
+SUM(items.price * items.quantity),
+2
+) AS receita
+
+FROM
+`bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+
+UNNEST(items) AS items
+
+WHERE
+event_name = 'purchase'
+
+GROUP BY
+items.item_name
+
+ORDER BY
+receita DESC
+
+LIMIT 10;
+
+
+
+-- Top 10 produtos por quantidade vendida
+
+SELECT
+
+items.item_name,
+
+SUM(items.quantity) AS quantidade_vendida
+
+FROM
+`bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+
+UNNEST(items) AS items
+
+WHERE
+event_name = 'purchase'
+
+GROUP BY
+items.item_name
+
+ORDER BY
+quantidade_vendida DESC
+
+LIMIT 10;
+
+
+
+-- Top 10 categorias por receita
+
+SELECT
+
+items.item_category,
+
+ROUND(
+SUM(items.price * items.quantity),
+2
+) AS receita
+
+FROM
+`bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+
+UNNEST(items) AS items
+
+WHERE
+event_name = 'purchase'
+
+GROUP BY
+items.item_category
+
+ORDER BY
+receita DESC
+
+LIMIT 10;
+
+
+
+-- Top 10 categorias por quantidade vendida
+
+SELECT
+
+items.item_category,
+
+SUM(items.quantity) AS quantidade_vendida
+
+FROM
+`bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
+UNNEST(items) AS items
+
+WHERE
+event_name = 'purchase'
+
+GROUP BY
+items.item_category
+
+ORDER BY
+quantidade_vendida DESC
+
+LIMIT 10;
