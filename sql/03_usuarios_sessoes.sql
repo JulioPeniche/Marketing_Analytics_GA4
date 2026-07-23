2b556f42fb464c48d0d5b1cb1b3f417299228695
Júlio Peniche <76125283+JulioPeniche@users.noreply.github.com>
2026-07-23 09:52:46 -0300

    Update 03_usuarios_sessoes.sql

--- sql/03_usuarios_sessoes.sql
+++ sql/03_usuarios_sessoes.sql
@@ -65,3 +65,52 @@ FROM
 GROUP BY data
 
 ORDER BY data;
+
+
+
+-- Novos usuários por dia
+
+SELECT
+
+  PARSE_DATE('%Y%m%d', event_date) AS data,
+
+  COUNT(DISTINCT user_pseudo_id) AS novos_usuarios
+
+FROM
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+
+WHERE
+  event_name = 'first_visit'
+
+GROUP BY data
+
+ORDER BY data;
+
+
+
+-- Média de eventos por sessão
+
+SELECT
+
+ROUND(
+    COUNT(*) /
+    COUNT(DISTINCT CONCAT(
+        user_pseudo_id,
+        CAST((
+            SELECT value.int_value
+            FROM UNNEST(event_params)
+            WHERE key = 'ga_session_id'
+        ) AS STRING)
+    )),
+2
+) AS media_eventos_por_sessao
+
+FROM
+`bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
+
+WHERE
+(
+SELECT value.int_value
+FROM UNNEST(event_params)
+WHERE key = 'ga_session_id'
+) IS NOT NULL;
